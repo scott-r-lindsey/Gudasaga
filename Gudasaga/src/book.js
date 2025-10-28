ae5993fe3e6b9707310ea5a59a593f962f2041f6
Scott Lindsey <scott@scott.to>
2025-10-27 21:46:22 -0700

    Berikade lådan med ting

--- Gudasaga/src/book.js
+++ Gudasaga/src/book.js
@@ -1,8 +1,12 @@
+const antalKapitel = 42;
+const kapitelLista = [];
+
+for (let kapitelIndex = 0; kapitelIndex < antalKapitel; kapitelIndex += 1) {
+  const indexText = String(kapitelIndex).padStart(2, '0');
+  kapitelLista.push(`./kapitel/kapitel_${indexText}.txt`);
+}
+
 export const book = {
   title: 'My Awesome Book',
-  chapters: [
-    './chapters/chapter1.txt',
-    './chapters/chapter2.txt',
-    './chapters/chapter3.txt',
-  ],
+  chapters: kapitelLista,
 };
